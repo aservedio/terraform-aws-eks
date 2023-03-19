237b383eb1400c9c8c0341927f57692a76c0c5ba
Anthony Servedio <aservedio@geekstuff.it>
2023-03-19 18:53:44 +0000

    unquote instance_types

--- modules/eks-managed-node-group/main.tf
+++ modules/eks-managed-node-group/main.tf
@@ -381,7 +381,7 @@ resource "aws_eks_node_group" "this" {
     create_before_destroy = true
     ignore_changes = [
       scaling_config[0].desired_size,
-      "instance_types",
+      instance_types,
     ]
   }
 
